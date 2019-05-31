5a39c51dd34d5252176be93510d3f50b03b8cca6
Lvcas <lucas.giuri@gmail.com>
2019-05-31 09:00:54 +0200

    [LG-1] adding gitignore

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -3,7 +3,6 @@ import './Header.scss';
 import Logo from '../../assets/icons/cabify.svg?react';
 
 const Header = () => {
-	
 	return (
 		<div className="header">
 			<Logo className="header__logo" alt="" />
